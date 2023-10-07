cf85043fe309ffe8964044ca576725e7bf64a40d
Ties de Kock <ties@tiesdekock.nl>
2023-10-07 17:51:02 +0200

    Move the fixture

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import logging
+import os
+import aiohttp
+import pytest
+
+from sagemcom_f3896_client.client import SagemcomModemSessionClient
+
+
+LOG = logging.getLogger(__name__)
+
+@pytest.fixture
+def client(event_loop):
+    """
+    Build a client using settings from environment variable, without requiring a context manager.
+    """
+    modem_url = os.environ.get("MODEM_URL", None)
+    if not modem_url:
+        LOG.info("MODEM_URL environment variable is not set, using default")
+        modem_url = "http://192.168.100.1"
+
+    modem_password = os.environ.get("MODEM_PASSWORD")
+    assert modem_password, "MODEM_PASSWORD environment variable is not set"
+
+    async def sessio():
+        return aiohttp.ClientSession()
+
+    session = event_loop.run_until_complete(sessio())
+    client = SagemcomModemSessionClient(session, modem_url, modem_password)
+    yield client
+
+    event_loop.run_until_complete(client._logout())
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,8 @@
 import collections
 import datetime
 import logging
-import os
 import random
 
-import aiohttp
 import pytest
 from pytest import LogCaptureFixture
 
@@ -13,29 +11,6 @@ from tests.util import requires_modem_password
 
 logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
-
-
-@pytest.fixture
-def client(event_loop):
-    """
-    Build a client using settings from environment variable, without requiring a context manager.
-    """
-    modem_url = os.environ.get("MODEM_URL", None)
-    if not modem_url:
-        LOG.info("MODEM_URL environment variable is not set, using default")
-        modem_url = "http://192.168.100.1"
-
-    modem_password = os.environ.get("MODEM_PASSWORD")
-    assert modem_password, "MODEM_PASSWORD environment variable is not set"
-
-    async def sessio():
-        return aiohttp.ClientSession()
-
-    session = event_loop.run_until_complete(sessio())
-    client = SagemcomModemSessionClient(session, modem_url, modem_password)
-    yield client
-
-    event_loop.run_until_complete(client._logout())
 
 
 @requires_modem_password()
