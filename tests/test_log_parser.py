3b29e3bb64771b3eac37ad48b980e8d61cb38508
Ties de Kock <ties@tiesdekock.nl>
2023-10-23 17:22:30 +0200

    Parse another event type

--- tests/test_log_parser.py
+++ tests/test_log_parser.py
@@ -9,6 +9,10 @@ from sagemcom_f3896_client.log_parser import (
 )
 
 LOG_MESSAGES = [
+    "CM-STATUS message sent. Event Type Code: 8; Chan ID: 27; DSID: N/A; MAC Addr: N/A; OFDM/OFDMA Profile ID: N/A.;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
+    "Unicast Maintenance Ranging attempted - No response - Retries exhausted;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
+    "16 consecutive T3 timeouts while trying to range on upstream channel 8;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
+    "Started Unicast Maintenance Ranging - No Response received - T3 time-out;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
     "CM-STATUS message sent. Event Type Code: 24; Chan ID: 33; DSID: N/A; MAC Addr: N/A; OFDM/OFDMA Profile ID: 3.;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
     "CM-STATUS message sent. Event Type Code: 16; Chan ID: 33; DSID: N/A; MAC Addr: N/A; OFDM/OFDMA Profile ID: 3.;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
     "US profile assignment change. US Chan ID: 27; Previous Profile: 10 13; New Profile: 9 13.;CM-MAC=44:05:a5:a5:a5:4a;CMTS-MAC=00:01:5c:de:ad:be;CM-QOS=1.1;CM-VER=3.1;",
@@ -47,6 +51,7 @@ LOG_MESSAGES = [
 
 
 def test_log_parser_integration_test():
+    """Test the log parser on the set of real messages."""
     types: Set[type] = set()
 
     for line in LOG_MESSAGES:
@@ -59,10 +64,16 @@ def test_log_parser_integration_test():
                 event_code=event_code,
                 profile=profile,
             ):
-                assert channel_id == 33
-                assert event_code in (16, 24)
-                assert profile == 3
-                assert ds_id is None
+                if event_code in (16, 24):
+                    assert channel_id == 33
+                    assert profile == 3
+                    assert ds_id is None
+                elif event_code == 8:
+                    assert channel_id == 8
+                    assert profile is None
+                    assert ds_id is None
+                else:
+                    assert False, "Unexpected event code"
             case DownstreamProfileMessage(
                 channel_id=channel_id,
                 previous_profile=previous_profile,
